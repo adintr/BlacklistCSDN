f2b1579a7117ad550f5b2252cb0e5cb40bb47ef6
adintr <webmaster@adintr.com>
2017-07-05 20:31:43 +0800

    显示和删除黑名单的功能

--- src/js/msglistener.js
+++ src/js/msglistener.js
@@ -1,13 +1,40 @@
 var global_black_list = [];
 
-if(localStorage["black_list"] && localStorage["black_list"] != "") {
-    global_black_list = JSON.parse(localStorage["black_list"]);
+Array.prototype.removeByValue = function(val) {
+    for(var i=0; i<this.length; i++) {
+        if(this[i] == val) {
+            this.splice(i, 1);
+            break;
+        }
+    }
+}
+
+function loadBloackList() {
+    if(localStorage["black_list"] && localStorage["black_list"] != "") {
+        global_black_list = JSON.parse(localStorage["black_list"]);
+    }
+}
+
+function saveBlackList() {
+    localStorage["black_list"] = JSON.stringify(global_black_list);
 }
 
 function add_black_user_and_save(username) {
     global_black_list.push(username);
-    localStorage["black_list"] = JSON.stringify(global_black_list);
+    saveBlackList();
 }
+
+function deleteBloackUser(username) {
+    if(confirm("从黑名单中删除 " + username + " ?")) {
+        global_black_list.removeByValue(username);
+        saveBlackList();
+        return true;
+    }
+
+    return false;
+}
+
+loadBloackList();
 
 chrome.extension.onRequest.addListener(
     function(request, sender, sendResponse) {
